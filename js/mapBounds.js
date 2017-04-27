9730d3c7a18da66746a4198538126e5bc36696ec
Javier Cancela <javier.cancela.vicente@gmail.com>
2017-04-27 17:36:48 +0200

    testing

--- js/mapBounds.js
+++ js/mapBounds.js
@@ -1,27 +1,6 @@
-var tiendas = [];
-var ciudades = [];
 var bounds = [];
 
-/*
-$.getJSON('json/tiendas.json', cargaTiendas).done(function() {
-    $.getJSON('json/ciudades.json', cargaCiudades).done(initMap);
-});
-*/
-
 $.getJSON('json/bounds.json', cargaBounds).done(initMap);
-
-
-function cargaTiendas(json) {
-    json.forEach(function (tienda) {
-        tiendas.push(tienda);
-    });
-};
-
-function cargaCiudades(json) {
-    json.forEach(function (ciudad) {
-        ciudades.push(ciudad);
-    });
-};
 
 function cargaBounds(json) {
     json.forEach(function (bound) {
@@ -29,73 +8,42 @@ function cargaBounds(json) {
     });
 };
 
-function getInitialMapOptions() {
-    // Initial position and zoom
-    var position = { lat: 43.362174, lng: -8.414679 };
-    var zoom = 7;
-    var mapOptions =  {
-        center: position,
-        zoom: zoom,
-        mapTypeId: google.maps.MapTypeId.SATELLITE
-    };
-    
-    return mapOptions;
-}
-
-function getMapOptions(listaCiudades, codProv = 15, mapTypeId = google.maps.MapTypeId.SATELLITE) {
-    // Initial position and zoom
-    var position = { lat: 43.362174, lng: -8.414679 };
-    var zoom = 7;
-
-/*    ciudades.forEach(function(ciudad) {
-        if (ciudad["codProv"] == codProv) {
-            position = ciudad["position"];
-            zoom = ciudad["zoom"];
-        }
-    });*/
-
-    var mapOptions =  {
-        center: position,
-        zoom: zoom,
-        mapTypeId: mapTypeId
-    };
-    
-    return mapOptions;
-}
 
 function initMap() {
-/*    if (tiendas.length == 0 || ciudades.length == 0) {
-        console.log("Ciudades o tiendas es nulo");
-        return; 
-    }*/
+    const northEastLat = 43.362174;
+    const northEastLng = -8.414679;
+    const southWestLat = 36.264384;
+    const southWestLng = 0.753471;
 
-    var mapOptions = getInitialMapOptions();
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    ne = new google.maps.LatLng({
+        lat: northEastLat,
+        lng: northEastLng
+    });
+    sw = new google.maps.LatLng({
+        lat: southWestLat,
+        lng: southWestLng
+    });
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        disableDefaultUI: true,        
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+
+    initialBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(36.264384,0.753471),
+        new google.maps.LatLng(43.362174, -8.414679)
+    );
+
+
+    map.fitBounds({ initialBounds });
 };
 
 function changeBounds(bounds) {
-map.fitBounds({
+/*map.fitBounds({
       east: bounds.northEastLng,
       north: bounds.northEastLat,
       south: bounds.southWestLat,
       west: bounds.southWestLng
-    });    
-}
-
-function changeZoom(newZoomLevel) {
-    var zoom = map.getZoom();
-    var incr = (zoom < newZoomLevel) ? 1 : -1;
-    map.setZoom(zoom + incr);
-    if (zoom + incr == newZoomLevel) {
-        clearInterval(this.timer);
-    }
+    });    */
 };
 
-function zoomMap(zoom) {
-    this.timer = setInterval(function () {
-        changeZoom(zoom);
-    }, 300);
-};
-
-//window.setTimeout(zoomMap, 3000, 14);
-//window.setTimeout(zoomMap, 3000, 7);
